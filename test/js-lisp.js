81c57babdcf3c8f931312c5f9fcd1b441333f2e9
Narjis Hachilif <hnarjis@gmail.com>
2014-11-27 23:01:42 +0100

    Implement first

--- test/js-lisp.js
+++ test/js-lisp.js
@@ -11,3 +11,9 @@ exports.testNestedCalls = function(test){
 	test.equal(jslisp.interpret(['+', 1, ['+', 1, 1]]), 3);
 	test.done();
 };
+
+exports.testMultiplyAndFirst = function(test){
+	test.expect(1);
+	test.equal(jslisp.interpret(['*', 2, ['first', 3, 8]]), 6);
+	test.done();
+};
