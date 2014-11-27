4116826e521170f38e695ac0e425c63b139d5102
André Laszlo <andre@laszlo.nu>
2014-11-27 17:38:25 +0100

    Implement nested function calls

--- test/js-lisp.js
+++ test/js-lisp.js
@@ -5,3 +5,9 @@ exports.testOperatorPlus = function(test){
 	test.equal(jslisp.interpret(['+', 1, 2]), 3);
 	test.done();
 };
+
+exports.testNestedCalls = function(test){
+	test.expect(1);
+	test.equal(jslisp.interpret(['+', 1, ['+', 1, 1]]), 3);
+	test.done();
+};
