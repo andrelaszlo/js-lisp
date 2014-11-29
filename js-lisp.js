1912848d4e4f8c74b10ad44c2990573ae5020d50
André Laszlo <andre@laszlo.nu>
2014-11-29 15:49:44 +0100

    Implement lambdas
    * Lambdas are not values yet, they only work if they are invoked immediately
    * Add underscore dependency for the handy zip function
    * Comment parts of the interpreter

--- js-lisp.js
+++ js-lisp.js
@@ -1,3 +1,5 @@
+var _ = require('underscore');
+
 var default_scope = {
 
     // Built-in function declarations. These all take two arguments: the
@@ -10,7 +12,7 @@ var default_scope = {
     },
     'head': function(scope, args) { // TODO: exception if empty list
         return args[0];
-    },
+    }
 }
 
 // Returns the parent scope of a scope
@@ -42,20 +44,50 @@ var interpret = function (prog, parent_scope) {
         parent_scope = default_scope;
     }
 
-    if (! (prog instanceof Array)) {
+    if (typeof prog == 'string') {
+        // A bound name, return it's actual value
+        return scope_lookup(parent_scope, prog);
+    } else if (! (prog instanceof Array)) {
+        // Some primitive value, like a number, will evaluate to itself
         return prog;
     }
+
     if (prog.length == 0) {
+        // The empty list (NIL) also evaluates to itself
         return [];
     }
 
     var fun_exp = prog[0];
 
-    if (fun_exp === 'lambda') {
-        var arglist = prog[1];
-        // TODO
+    if (_.isArray(fun_exp) && fun_exp[0] === 'lambda') {
+        // TODO: this only handles lambdas that are being invoked immediately
+        var formal_parameters = fun_exp[1];
+        var lambda_body = fun_exp[2];
+
+        var lambda = function() {
+            var args = Array.prototype.slice.call(arguments);
+            if (formal_parameters.length != args.length) {
+                throw new Error("Wrong number of args, " + args.length + " instead of " + formal_parameters.length);
+            }
+
+            // Bind the parameters to variables in the local scope.
+            var scope = {};
+            _.zip(formal_parameters, args).forEach(
+                function(binding) {
+                    scope[binding[0]] = binding[1];
+                });
+            scope_push(parent_scope, scope);
+
+            return interpret(lambda_body, scope);
+        };
+
+        var actual_parameters = prog.slice(1).map(function(param) {
+            return interpret(param, parent_scope);
+        });
+
+        return lambda.apply(null, actual_parameters);
     } else if (typeof fun_exp == 'string') {
-        // Built-in or regular function
+        // Built-in or named function
         var fun = scope_lookup(parent_scope, fun_exp);
         var args = prog.slice(1).map(function(arg) {
             return interpret(arg, parent_scope);
