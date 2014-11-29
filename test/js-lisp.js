2fd24518897d1e6a14c1b1f01b938d3479ffd211
André Laszlo <andre@laszlo.nu>
2014-11-29 15:49:44 +0100

    Implement lambdas
    * Lambdas are not values yet, they only work if they are invoked immediately
    * Add underscore dependency for the handy zip function
    * Comment parts of the interpreter

--- test/js-lisp.js
+++ test/js-lisp.js
@@ -1,12 +1,16 @@
 var jslisp = require('../js-lisp');
 
+/*
+ Test helper function. Checks that an expression returns the expected value
+ when evaluated.
+ */
 var interpretTest = function(expression, expected) {
-  return function(test) {
-    test.expect(1);
-    test.equal(jslisp.interpret(expression), expected);
-    test.done();
-  }
-}
+    return function(test) {
+        test.expect(1);
+        test.equal(jslisp.interpret(expression), expected);
+        test.done();
+    };
+};
 
 exports.testOperatorPlus = interpretTest(['+', 1, 2], 3);
 
@@ -14,5 +18,8 @@ exports.testNestedCalls = interpretTest(['+', 1, ['+', 1, 1]], 3);
 
 exports.testMultiplyAndHead = interpretTest(['*', 2, ['head', 3, 8]], 6);
 
-//exports.testSimpleLambda = interpretTest([['lambda', ['x', 'y'], ['*', 'x', 'y']], 2, 3], 6);
+exports.testSimpleLambda = interpretTest(
+    [['lambda', ['x', 'y'], ['*', 'x', 'y']],
+     2, 3],
+    6);
 
