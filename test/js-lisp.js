802e2e593379e53d32db163339c5c6d60ca850b9
hnarjis <hnarjis@gmail.com>
2014-12-01 17:59:04 +0100

    Implement tail

--- test/js-lisp.js
+++ test/js-lisp.js
@@ -4,7 +4,14 @@ var jslisp = require('../js-lisp');
  Test helper function. Checks that an expression returns the expected value
  when evaluated.
  */
-var interpretTest = function(expression, expected) {
+var interpretTest = function(expression, expected, deepEqual) {
+    if (deepEqual === true) {
+        return function(test) {
+            test.expect(1);
+            test.deepEqual(jslisp.interpret(expression), expected);
+            test.done();
+        };
+    };	
     return function(test) {
         test.expect(1);
         test.equal(jslisp.interpret(expression), expected);
@@ -19,6 +26,8 @@ exports.testOperatorMultiplication = interpretTest(['*', 3, 4], 12);
 exports.testNestedCalls = interpretTest(['+', 1, ['+', 1, 1]], 3);
 
 exports.testMultiplyAndHead = interpretTest(['*', 2, ['head', 3, 8]], 6);
+
+exports.testTail = interpretTest(['tail', 3, 8, 6], [8, 6], true);
 
 exports.testSimpleLambda = interpretTest(
     [['lambda', ['x', 'y'], ['*', 'x', 'y']],
