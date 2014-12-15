975d1ad29ae8124c33a3e109cedd7677b4d984f8
enarhac <narjis.hachilif@ericsson.com>
2014-12-15 16:08:13 +0100

    Implement relational operators

--- definitions.js
+++ definitions.js
@@ -20,16 +20,19 @@ var global_scope = {
         return args.reduce(function(a, b){ return a/b; });
     },
     '<': function() {
-        var args = args_as_array(arguments);
-        if (args.length < 2) {
-            return true;
-        }
-        for (var i = 0; i < args.length-1; i++) {
-            if (args[i] >= args[i+1]) {
-                return false;
-            }
-        }
-        return true;
+        return compare_args(function(x, y) { return x < y; }, args_as_array(arguments));
+    },
+    '>': function() {
+        return compare_args(function(x, y) { return x > y; }, args_as_array(arguments));
+    },
+    '=': function() {
+        return compare_args(function(x, y) { return x == y; }, args_as_array(arguments));
+    },
+    '<=': function() {
+        return compare_args(function(x, y) { return x <= y; }, args_as_array(arguments));
+    },
+    '>=': function() {
+        return compare_args(function(x, y) { return x >= y; }, args_as_array(arguments));
     },
     'head': function() { // TODO: exception if empty list
         return args_as_array(arguments)[0];
@@ -44,6 +47,18 @@ var global_scope = {
         return args_as_array(arguments).slice(-1);
     }
 };
+
+var compare_args = function(cmp, args) {
+    if (args.length < 2) {
+        return true;
+    }
+    for (var i = 0; i < args.length-1; i++) {
+        if (!cmp(args[i], args[i+1])) {
+            return false;
+        }
+    }
+    return true;
+}
 
 /*
  * Bind all the names from Math as `js` forms or values in the global scope.
